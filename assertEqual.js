ad20f4fb9f39f2f5c45d26a90bfb31ddc7cbd254
William Lionhart <linkwithlionhart@gmail.com>
2023-07-11 21:31:28 -0700

    Complete function and test cases

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+// TEST CODE
+assertEqual('LHL', 'LHL')
+assertEqual('Lighthouse Labs', 'Bootcamp')
+assertEqual(1, 1)
+assertEqual(1, 2)
