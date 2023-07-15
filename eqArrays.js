424333e80622180a32e244bd0888eb887d033b0c
linkwithlionhart <linkwithlionhart@gmail.com>
2023-07-15 14:02:44 -0700

    Complete implementation

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (getArray1, getArray2) => {
+  if (getArray1.length !== getArray2.length) {
+    return false;
+  } // end if
+
+  for (let i = 0; i < getArray1.length; i++) {
+    if (getArray1[i] !== getArray2[i]) {
+      return false;
+    } // end if
+  } // end for loop
+
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
