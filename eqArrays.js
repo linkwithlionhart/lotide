2a420b77d82abdfb3a5cbd2f1dd41bf2ab3c7e54
linkwithlionhart <linkwithlionhart@gmail.com>
2023-07-17 21:53:24 -0700

    Changes nominal

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// FUNCTIONS COPIED
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,19 +7,22 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+// FUNCTION IMPLEMENTATION
 const eqArrays = (getArray1, getArray2) => {
   if (getArray1.length !== getArray2.length) {
     return false;
   } // end if
 
-  for (let i = 0; i < getArray1.length; i++) {
-    if (getArray1[i] !== getArray2[i]) {
-      return false;
-    } // end if
-  } // end for loop
+  for (let x = 0; x < getArray1.length; x++) {
+    for (let y = 0; y < getArray2.length; y++) {
+      if (x === y && getArray1[x] !== getArray2[y]) {
+        return false;
+      } // end if
+    } // end for loop 2
+  } // end for loop 1
 
   return true;
-};
+} // end function
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
