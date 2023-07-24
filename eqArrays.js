c2e6d0063eb5afd8046a8b89309468232925244f
linkwithlionhart <linkwithlionhart@gmail.com>
2023-07-24 05:49:25 +0000

    Refactor code for robustness

--- eqArrays.js
+++ eqArrays.js
@@ -8,21 +8,22 @@ const assertEqual = function (actual, expected) {
 };
 
 // FUNCTION IMPLEMENTATION
-const eqArrays = (getArray1, getArray2) => {
-  if (getArray1.length !== getArray2.length) {
-    return false;
-  } // end if
-
-  for (let x = 0; x < getArray1.length; x++) {
-    for (let y = 0; y < getArray2.length; y++) {
-      if (x === y && getArray1[x] !== getArray2[y]) {
-        return false;
-      } // end if
-    } // end for loop 2
-  } // end for loop 1
-
+const eqArrays = (arr1, arr2) => {
+  // First, check if the arrays have the same length
+  if (arr1.length !== arr2.length) {
+      return false;
+  }
+  
+  // Next, check each pair of elements
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+          return false;
+      }
+  }
+  
+  // If we haven't returned false yet, then the arrays must be equal
   return true;
-} // end function
+}
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
