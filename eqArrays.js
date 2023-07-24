8da2d404eda78a0f6e04ae927ffd0e4029407813
linkwithlionhart <linkwithlionhart@gmail.com>
2023-07-24 00:24:19 -0700

    Refactor code

--- eqArrays.js
+++ eqArrays.js
@@ -9,21 +9,17 @@ const assertEqual = function (actual, expected) {
 
 // FUNCTION IMPLEMENTATION
 const eqArrays = (arr1, arr2) => {
-  // First, check if the arrays have the same length
   if (arr1.length !== arr2.length) {
-      return false;
+    return false;
   }
-  
   // Next, check each pair of elements
   for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-          return false;
-      }
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
-  
-  // If we haven't returned false yet, then the arrays must be equal
   return true;
-}
+} // end function: eqArrays
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
