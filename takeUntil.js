70af1e718b5358183d5bca8564026eb80fd33dbf
linkwithlionhart <linkwithlionhart@gmail.com>
2023-07-24 23:39:53 -0700

    Implement function and pass test cases

---
+++ takeUntil.js
@@ -0,0 +1,23 @@
+// Implement function: takeUntil
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+} // end funciton: takeUntil
+
+// Test Cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); // Expected output: [ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2); // Expected output: [ 'I\'ve', 'been', 'to', 'Hollywood' ]
