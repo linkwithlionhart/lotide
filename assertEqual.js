36da2a02d952bd15cd17de6a2eb489d1c23259f1
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-03 22:42:51 -0700

    Refactor and deprecate code

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,20 @@
 // FUNCTION IMPLEMENTATION
+// Refactored
+const assertEqual = function(actual, expected) {
+  let message;
+  if (actual === expected) {
+    message = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(message);
+};
+
+// MODULE: EXPORT
+module.exports = assertEqual;
+
+/*
+// Outdated
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
@@ -8,7 +24,8 @@ const assertEqual = function (actual, expected) {
 } // end function: assertEqual
 
 // TEST CODE
-assertEqual('LHL', 'LHL')
-assertEqual('Lighthouse Labs', 'Bootcamp')
-assertEqual(1, 1)
-assertEqual(1, 2)
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('😀', '😀');
+assertEqual('😀', '😂');
+*/
