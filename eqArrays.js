6dca4f3bcde1d87bfa841e353092c218c59a0bed
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-04 00:01:40 -0700

    Refactor code and pass tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// FUNCTIONS COPIED
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // FUNCTION IMPLEMENTATION
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
@@ -21,8 +12,22 @@ const eqArrays = (arr1, arr2) => {
   return true;
 } // end function: eqArrays
 
+// MODULE: EXPORT
+module.exports = eqArrays;
+
+/* Outdated
+// FUNCTIONS COPIED
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+*/
