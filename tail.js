b69486d4992829ee91baf78fd71cfac9ff0bb1e4
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-04 00:01:19 -0700

    Refactor code and pass tests

--- tail.js
+++ tail.js
@@ -1,12 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+// Refactored
+const tail = array => array.slice(1);
 
+// MODULE: EXPORT
+module.exports = tail;
+
+/* Outdated
 const tail = function (getArray) {
   const newArray = []
   for (let i = 0; i <= getArray.length - 1; i++) {
@@ -17,16 +16,31 @@ const tail = function (getArray) {
   return (newArray)
 } // end function: tail()
 
-// Test Case: Check the original array
-const words = ['Yo Yo', 'Lighthouse', 'Labs']
-tail(words) // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3) // original array should still have 3 elements!
-assertEqual(tail(words).length, 2) // expected value is 2
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
 
-// Test Case: An array with only one element should yield an empty array for its tail
-const words1 = ['Yo Yo']
-assertEqual(tail(words1).length, 0)
+// Test cases for the tail function
+// Test Case 1: Checking the returned array
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // Should pass
 
-// Test Case: An empty array should yield an empty array for its tail
-const words2 = []
-assertEqual(tail(words2).length, 0)
+// Test Case 2: Checking if the original array remains unmodified
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); 
+assertEqual(words.length, 3); // Should pass
+
+// Test Case 3: An array with only one element should yield an empty array for its tail
+const singleElement = ["Hello"];
+const singleResult = tail(singleElement);
+assertEqual(singleResult, []); // Should pass
+
+// Test Case 4: An empty array should yield an empty array for its tail
+const emptyArray = [];
+const emptyResult = tail(emptyArray);
+assertEqual(emptyResult, []); // Should pass
+*/
